e12415502c3b923bf84829ec4fe9c90c599ad6fa
RandomDummyAccount <chnrm7@gmail.com>
2023-08-29 16:10:07 -0500

    Update answers.sql

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -1,1 +1,12 @@
+--	Write a query to get the sum of impressions by day.
+SELECT impressions, date
+FROM marketing_performance;
 
+
+--	Write a query to get the top three revenue-generating states in order of best to worst. How much revenue did the third best state generate?
+
+--	Write a query that shows total cost, impressions, clicks, and revenue of each campaign. Make sure to include the campaign name in the output.
+
+--	Write a query to get the number of conversions of Campaign5 by state. Which state generated the most conversions for this campaign?
+
+--	In your opinion, which campaign was the most efficient, and why?
